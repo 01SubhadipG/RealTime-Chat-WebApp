c440a071631ec32c7dd433fa049d046544154c29
SUBHADIP GHOSH <subhadipghosh977@gmail.com>
2026-03-07 15:18:48 +0530

    modified

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,12 +1,15 @@
 import jwt from 'jsonwebtoken';
 
-export const generateToken = (userId,res) => {
+export const generateToken = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    
     res.cookie('jwt', token, {
-        httpOnly: true,//prevent client-side JS from accessing the cookie xss attack cross-site scripting attacks
-        secure: process.env.NODE_ENV !== 'development',//https
-        sameSite: 'strict',//crsf cross-site request forgery attacks
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true, 
+        // For Render (Cross-site), these two must be set like this:
+        secure: true, 
+        sameSite: 'none', 
     });
+
     return token;
 }
